d36b4ede1e6e89352a5236f6c22f161619bd15e9
shivam <shivamfeb77@gmail.com>
2026-07-02 19:53:09 +0530

    remove target folder from git track

--- backend/auth-service/src/main/java/com/queuemgmt/auth/entity/User.java
+++ backend/auth-service/src/main/java/com/queuemgmt/auth/entity/User.java
@@ -14,13 +14,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
